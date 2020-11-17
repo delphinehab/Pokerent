7629e2c1639d9f610b7516bedb7d40cc76abb70c
Antoine Goeuriot <antoine.goeuriot@gmail.com>
2020-11-17 19:00:05 +0100

    debugged

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,7 +2,7 @@ class PokemonsController < ApplicationController
 
   def index
     if params[:query].present?
-      @pokemons = policty_scope(Pokemon.where("name ILIKE ?", "%#{params[:query]}%"))
+      @pokemons = policy_scope(Pokemon.where("name ILIKE ?", "%#{params[:query]}%"))
     else
       @pokemons = policy_scope(Pokemon)
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_11_17_153501) do
-
+ActiveRecord::Schema.define(version: 2020_11_17_161800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

09fd2e3d43fdf9aab1c7f89c4d46675f80dd0b29
Antoine Goeuriot <antoine.goeuriot@gmail.com>
2020-11-17 12:19:11 +0100

    added show page and started new action pokemons

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -6,6 +6,25 @@ class PokemonsController < ApplicationController
 
   def show
     @pokemon = Pokemon.find(params[:id])
+    @reviews = Review.all
   end 
+
+  def new
+    @pokemon = Pokemon.new
+  end
+  
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    @pokemon.user = current_user
+    @pokemon.save
+    raise
+    redirect_to pokemon_path(@pokemon)
+  end 
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name,:description,:element,:evolution_level)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pokemons#index'
   resources :pokemons
+  resources :pokemons do
+    resources :bookings
+  end
+  resources :bookings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

0ed7d10d7aedc9a15b79ffc0486379061e75debb
Rodolphe Vacher <rodolphe.vacher@edu.emlyon.com>
2020-11-17 15:33:45 +0100

    created a new pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,6 @@
 class Pokemon < ApplicationRecord
-  ELEMENTS = %W(Fire Grass Water Electricity)
+  ELEMENTS = %W(fire grass water electricity)
+  LEVELS = [1, 2, 3]
 
   belongs_to :user
   has_many :bookings, dependent: :destroy

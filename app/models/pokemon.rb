3b2014495374223d8f8996f77856796812ccd9da
Tanguy Truhe <tanguy.clarence@gmail.com>
2020-11-17 12:45:37 +0100

    fixed

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,8 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
+<<<<<<< HEAD
   has_many :reviews
+=======
+  has_many :bookings
+>>>>>>> 75d8b4b740d0290ee9f7e588cf020ef37a281530
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pokemons#index'
 
+<<<<<<< HEAD
   resources :pokemons do
     resources :reviews, only: [ :new, :create ]
   end
+=======
+  resources :pokemons
+  
+  resources :pokemons do
+    resources :bookings, only: [:new, :create]
+  end
+  
+  resources :bookings
+
+  resources :users do
+    resources :bookings, only: [:index]
+  end
+
+>>>>>>> 75d8b4b740d0290ee9f7e588cf020ef37a281530
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

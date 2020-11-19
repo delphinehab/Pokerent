7a00012f286578615ca252d597b350548fd9b9bc
Rodolphe Vacher <rodolphe.vacher@edu.emlyon.com>
2020-11-19 16:22:51 +0100

    can't change password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
+    params.require(:user).permit(:email, :firstname, :lastname)
   end
 end

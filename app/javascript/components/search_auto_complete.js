75c70326c97cebe010ff14b37900a55dd8fec7c5
Tanguy Truhe <tanguy.clarence@gmail.com>
2020-11-20 15:49:57 +0100

    finish search bar

--- app/javascript/components/search_auto_complete.js
+++ app/javascript/components/search_auto_complete.js
@@ -1,11 +1,30 @@
 
+const searchAuto = () => {
+  const input = document.getElementById('myInput');
+  const pokemons = JSON.parse(input.dataset.pokemons);
+  const pokemonsCard = Array.from(document.querySelectorAll("[data-name]"));
+  console.log(input.value);
+  console.log(pokemons.class);
 
-const searchAuto
-  const input = document.querySelector()
-  const pokemons = JSON.parse(input.dataset.pokemons)
-  const pokemons =
-
-
+  input.addEventListener("keyup", event => {
+    pokemonsCard.forEach((card) => {
+      card.classList.remove('d-none')
+    });
+    console.log(pokemonsCard)
+    const matchingPokemons = pokemonsCard.filter((card) => {
+      return card.dataset.name.match(input.value) ? false : true
+      // return card.match(input.value) ? true : false
+    });
+    console.log(matchingPokemons)
+    matchingPokemons.forEach((card) => {
+      card.classList.add('d-none')
+    });
+  });
+}
 
 
 export { searchAuto }
+
+
+
+
